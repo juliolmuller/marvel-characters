2e264499e7a68ffad9596ef1eda7a29399ce41d1
Julio Muller <juliodolago@gmail.com>
2022-08-10 22:25:06 -0300

    feat: update React root for new React v18 API

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,8 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './components/App'
 import 'jquery'
 import 'bootstrap'
 import './assets/styles/global.scss'
 
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById('root'),
-)
+createRoot(document.getElementById('root')!).render(<App />)
